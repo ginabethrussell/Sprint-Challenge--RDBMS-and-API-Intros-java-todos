92a2ebad5a0d4fa131fb7dc05546f18b2abb75e7
ginabethrussell <ginabeth.russell@gmail.com>
2021-02-19 10:12:37 -0500

    added ability to save todos from seed data in UserServiceImpl

--- src/main/java/com/lambdaschool/todos/services/UserServiceImpl.java
+++ src/main/java/com/lambdaschool/todos/services/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.lambdaschool.todos.services;
 
+import com.lambdaschool.todos.models.Todos;
 import com.lambdaschool.todos.models.User;
 import com.lambdaschool.todos.repository.UserRepository;
 import com.lambdaschool.todos.views.UserNameCountTodos;
@@ -70,6 +71,15 @@ public class UserServiceImpl implements UserService
         newUser.setPassword(user.getPassword());
         newUser.setPrimaryemail(user.getPrimaryemail()
             .toLowerCase());
+
+        newUser.getTodos()
+            .clear();
+        for (Todos todo : user.getTodos())
+        {
+            newUser.getTodos()
+                .add(new Todos(newUser,
+                    todo.getDescription()));
+        }
 
         return userrepos.save(newUser);
     }
